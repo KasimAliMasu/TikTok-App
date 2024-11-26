7cbb1758138807f5219f6b94e83b3d793c755f37
KasimAliMasu <kasimalimasu@gmail.com>
2024-11-26 15:19:11 +0530

    connect with firebase

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,3 +1,28 @@
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'dart:io';
 
-class AuthController extends GetxController {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:tiktok_app/constants.dart';
+
+class AuthController extends GetxController {
+  void registerUser(
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error Creating Account',
+        e.toString(),
+      );
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tiktok_app/constants.dart';
+import 'package:tiktok_app/controllers/auth_controller.dart';
 import 'package:tiktok_app/views/screens/auth/signup_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
