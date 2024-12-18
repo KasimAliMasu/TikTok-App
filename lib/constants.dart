21239e6118c868807b37a42682de9d4d452d02c8
KasimAliMasu <kasimalimasu@gmail.com>
2024-12-18 16:58:32 +0530

    modified the code and add the files

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_app/controllers/auth_controller.dart';
+import 'package:tiktok_app/views/screens/add_video_screen.dart';
+
+List pages = [
+  Text('Home Screen'),
+  Text('Search Screen'),
+  const AddVideoScreen(),
+  Text('message Screen'),
+  Text('profile Screen'),
+];
 
 // COLORS
 const backgroundColor = Colors.black;
